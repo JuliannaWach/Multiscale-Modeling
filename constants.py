3e1acd7f635191d56025ce0fb8ab8d16d06fc8a2
JuliannaWach <juliannawach@gmail.com>
2026-05-28 23:44:39 +0200

    Konfiguracja - rozmiar okna, kolory, FPS

---
+++ constants.py
@@ -0,0 +1,23 @@
+# --- Rozmiar obszaru siatki
+CANVAS_WIDTH = 700
+CANVAS_HEIGHT = 780
+
+# --- Panel menu (rozmiar po prawej stronie okienka)
+MENU_WIDTH = 260 # --- Wartosc dowolna, taka sobie narzucilam "tak o"
+
+# --- Rozmiar calego okienka GUI
+WINDOW_WIDTH = CANVAS_WIDTH + MENU_WIDTH
+WINDOW_HEIGHT = CANVAS_HEIGHT
+
+# --- Predkosc odswiezania okienka
+FPS = 30 # --- Wartosc dowolna, taka sobie narzucilam "tak o"
+
+# --- Kolorki (rozszerzona paleta wzgledem poprzedniego cwiczenia - MultiscaleModeling)
+WHITE = (255, 255, 255)
+BLACK = (0, 0 , 0)
+GREY = (180, 180, 180)
+LIGHT_GREY = (220, 220, 220)
+DARK_GREY = (60, 60, 60)
+GREEN = (55, 155, 55)
+RED = (200, 55, 55)
+PANEL_BACKGROUND_COLOR = (235, 235, 235)
